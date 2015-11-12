e1dda4ef07a3e0c4f8488d092298bcc5ec39ed26
Kenny Goff <goff.kenneth@gmail.com>
2015-11-12 13:52:55 -0500

    Trailing slashes in urls

--- box_auth/boxusers/urls.py
+++ box_auth/boxusers/urls.py
@@ -5,10 +5,10 @@ from box_auth.boxusers import views
 urlpatterns = patterns(
     '',
     # Examples:
-    url(regex=r'^auth$',
+    url(regex=r'^auth/$',
         view=views.BoxAuth.as_view(),
         name='auth'),
-    url(regex=r'^auth-confirm$',
+    url(regex=r'^auth-confirm/$',
         view=views.BoxAuthConfirm.as_view(),
         name='auth-confirm'),
 )
